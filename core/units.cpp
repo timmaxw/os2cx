e5d4382f75ae161f280399552630f103a9a93ba7
Tim Maxwell <timmaxw@gmail.com>
2018-07-13 07:23:15 -0700

    [wip] unit system logic

--- core/units.cpp
+++ core/units.cpp
@@ -8,47 +8,80 @@
 
 namespace os2cx {
 
-const UnitType UnitType::dimensionless        ( 0,  0,  0,  0,  0,  0,  0);
+UnitType UnitType::dimensionless() {
+    return UnitType( 0,  0,  0,  0,  0,  0,  0); }
 
-const UnitType UnitType::length               ( 1,  0,  0,  0,  0,  0,  0);
-const UnitType UnitType::mass                 ( 0,  1,  0,  0,  0,  0,  0);
-const UnitType UnitType::time                 ( 0,  0,  1,  0,  0,  0,  0);
-const UnitType UnitType::temperature          ( 0,  0,  0,  1,  0,  0,  0);
-const UnitType UnitType::electric_current     ( 0,  0,  0,  0,  1,  0,  0);
-const UnitType UnitType::amount_of_substance  ( 0,  0,  0,  0,  0,  1,  0);
-const UnitType UnitType::luminous_intensity   ( 0,  0,  0,  0,  0,  0,  1);
+UnitType UnitType::length() {
+    return UnitType( 1,  0,  0,  0,  0,  0,  0); }
+UnitType UnitType::mass() {
+    return UnitType( 0,  1,  0,  0,  0,  0,  0); }
+UnitType UnitType::time() {
+    return UnitType( 0,  0,  1,  0,  0,  0,  0); }
+UnitType UnitType::electric_current() {
+    return UnitType( 0,  0,  0,  0,  1,  0,  0); }
+UnitType UnitType::thermodynamic_temperature() {
+    return UnitType( 0,  0,  0,  1,  0,  0,  0); }
+UnitType UnitType::amount_of_substance() {
+    return UnitType( 0,  0,  0,  0,  0,  1,  0); }
+UnitType UnitType::luminous_intensity() {
+    return UnitType( 0,  0,  0,  0,  0,  0,  1); }
 
-const UnitType UnitType::acceleration         ( 1,  0, -2,  0,  0,  0,  0);
-const UnitType UnitType::area                 ( 2,  0,  0,  0,  0,  0,  0);
-const UnitType UnitType::capacitance          (-2, -1,  4,  0,  2,  0,  0);
-const UnitType UnitType::density              (-3,  1,  0,  0,  0,  0,  0);
-const UnitType UnitType::dynamic_viscosity    (-1,  1, -1,  0,  0,  0,  0);
-const UnitType UnitType::electric_charge      ( 0,  0,  1,  0,  1,  0,  0);
-const UnitType UnitType::electric_conductance (-2, -1,  3,  0,  2,  0,  0);
-const UnitType UnitType::electric_potential   ( 2,  1, -3,  0, -1,  0,  0);
-const UnitType UnitType::electric_resistance  ( 2,  1, -3,  0, -2,  0,  0);
-const UnitType UnitType::energy               ( 2,  1, -2,  0,  0,  0,  0);
-const UnitType UnitType::force                ( 1,  1, -2,  0,  0,  0,  0);
-const UnitType UnitType::frequency            ( 0,  0, -1,  0,  0,  0,  0);
-const UnitType UnitType::inductance           ( 2,  1, -2,  0, -2,  0,  0);
-const UnitType UnitType::kinematic_viscosity  ( 2,  0, -1,  0,  0,  0,  0);
-const UnitType UnitType::magnetic_flux        ( 2,  1, -2,  0, -1,  0,  0);
-const UnitType UnitType::magnetic_flux_density( 0,  1, -2,  0, -1,  0,  0);
-const UnitType UnitType::power                ( 2,  1, -3,  0,  0,  0,  0);
-const UnitType UnitType::pressure             (-1,  1, -2,  0,  0,  0,  0);
-const UnitType UnitType::speed                ( 1,  0, -1,  0,  0,  0,  0);
-const UnitType UnitType::volume               ( 3,  0,  0,  0,  0,  0,  0);
+UnitType UnitType::acceleration() {
+    return UnitType( 1,  0, -2,  0,  0,  0,  0); }
+UnitType UnitType::area() {
+    return UnitType( 2,  0,  0,  0,  0,  0,  0); }
+UnitType UnitType::capacitance() {
+    return UnitType(-2, -1,  4,  0,  2,  0,  0); }
+UnitType UnitType::density() {
+    return UnitType(-3,  1,  0,  0,  0,  0,  0); }
+UnitType UnitType::dynamic_viscosity() {
+    return UnitType(-1,  1, -1,  0,  0,  0,  0); }
+UnitType UnitType::electric_charge() {
+    return UnitType( 0,  0,  1,  0,  1,  0,  0); }
+UnitType UnitType::electric_conductance() {
+    return UnitType(-2, -1,  3,  0,  2,  0,  0); }
+UnitType UnitType::electric_potential() {
+    return UnitType( 2,  1, -3,  0, -1,  0,  0); }
+UnitType UnitType::electric_resistance() {
+    return UnitType( 2,  1, -3,  0, -2,  0,  0); }
+UnitType UnitType::energy() {
+    return UnitType( 2,  1, -2,  0,  0,  0,  0); }
+UnitType UnitType::force() {
+    return UnitType( 1,  1, -2,  0,  0,  0,  0); }
+UnitType UnitType::frequency() {
+    return UnitType( 0,  0, -1,  0,  0,  0,  0); }
+UnitType UnitType::inductance() {
+    return UnitType( 2,  1, -2,  0, -2,  0,  0); }
+UnitType UnitType::kinematic_viscosity() {
+    return UnitType( 2,  0, -1,  0,  0,  0,  0); }
+UnitType UnitType::magnetic_flux() {
+    return UnitType( 2,  1, -2,  0, -1,  0,  0); }
+UnitType UnitType::magnetic_flux_density() {
+    return UnitType( 0,  1, -2,  0, -1,  0,  0); }
+UnitType UnitType::power() {
+    return UnitType( 2,  1, -3,  0,  0,  0,  0); }
+UnitType UnitType::pressure() {
+    return UnitType(-1,  1, -2,  0,  0,  0,  0); }
+UnitType UnitType::speed() {
+    return UnitType( 1,  0, -1,  0,  0,  0,  0); }
+UnitType UnitType::volume() {
+    return UnitType( 3,  0,  0,  0,  0,  0,  0); }
+
+UnitType UnitType::celsius_fahrenheit_temperature() {
+    return UnitType( 0,  0,  0,  1,  0,  0,  0, true); }
 
 UnitType::UnitType(
-    int lp, int mp, int tp, int ecp, int tep, int aosp, int lip
+    int lp, int mp, int tp, int ecp, int ttp, int aosp, int lip,
+    bool az
 ) :
     length_power(lp),
     mass_power(mp),
     time_power(tp),
     electric_current_power(ecp),
-    temperature_power(tep),
+    thermodynamic_temperature_power(ttp),
     amount_of_substance_power(aosp),
-    luminous_intensity_power(lip)
+    luminous_intensity_power(lip),
+    arbitrary_zero(az)
     { }
 
 bool UnitType::operator==(const UnitType &other) const {
@@ -56,90 +89,115 @@ bool UnitType::operator==(const UnitType &other) const {
         && mass_power == other.mass_power
         && time_power == other.time_power
         && electric_current_power == other.electric_current_power
-        && temperature_power == other.temperature_power
+        && thermodynamic_temperature_power ==
+            other.thermodynamic_temperature_power
         && amount_of_substance_power == other.amount_of_substance_power
-        && luminous_intensity_power == other.luminous_intensity_power;
+        && luminous_intensity_power == other.luminous_intensity_power
+        && arbitrary_zero == other.arbitrary_zero;
 }
 
 bool UnitType::operator!=(const UnitType &other) const {
     return !(*this == other);
 }
 
-const std::map<std::string, int> si_prefix_exponents {
-    {"y",  -24}, {"z",  -21}, {"a",  -18}, {"f",  -15},
-    {"p",  -12}, {"n",   -9}, {"u",   -6}, {"m",   -3},
-    {"c",   -2}, {"d",   -1}, {"da",  +1}, {"h",   +2},
-    {"k",   +3}, {"M",   +6}, {"G",   +9}, {"T",  +12},
-    {"P",  +15}, {"E",  +18}, {"Z",  +21}, {"Y",  +24}
+class UnitTable {
+public:
+    UnitTable(const std::vector<Unit> &au) : all_units(au) {
+        for (const Unit &unit : all_units) {
+            units_by_abbrev.insert(std::make_pair(unit.abbrev, unit));
+        }
+    }
+    std::vector<Unit> all_units;
+    std::map<std::string, Unit> units_by_abbrev;
 };
 
-const std::map<std::string, Unit> unit_values {
-    /* Fundamental SI units */
-    {"m",    Unit(UnitType::length,                1)},
-    {"g",    Unit(UnitType::mass,                  1.0e-3)},
-    {"s",    Unit(UnitType::time,                  1)},
-    {"K",    Unit(UnitType::temperature,           1)},
-    {"A",    Unit(UnitType::electric_current,      1)},
-    {"mol",  Unit(UnitType::amount_of_substance,   1)},
-    {"cd",   Unit(UnitType::luminous_intensity,    1)},
+const UnitTable &unit_table() {
+    static UnitTable ut({
+        Unit("m",    UnitType::length,                    1),
+        Unit("g",    UnitType::mass,                      1.0e-3),
+        Unit("s",    UnitType::time,                      1),
+        Unit("A",    UnitType::electric_current,          1),
+        Unit("K",    UnitType::thermodynamic_temperature, 1),
+        Unit("mol",  UnitType::amount_of_substance,       1),
+        Unit("cd",   UnitType::luminous_intensity,        1),
 
-    /* Other SI units */
-    {"F",    Unit(UnitType::capacitance,           1)},
-    {"C",    Unit(UnitType::electric_charge,       1)},
-    {"S",    Unit(UnitType::electric_conductance,  1)},
-    {"V",    Unit(UnitType::electric_potential,    1)},
-    {"ohm",  Unit(UnitType::electric_resistance,   1)},
-    {"J",    Unit(UnitType::energy,                1)},
-    {"Hz",   Unit(UnitType::frequency,             1)},
-    {"N",    Unit(UnitType::force,                 1)},
-    {"H",    Unit(UnitType::inductance,            1)},
-    {"Wb",   Unit(UnitType::magnetic_flux,         1)},
-    {"T",    Unit(UnitType::magnetic_flux_density, 1)},
-    {"Pa",   Unit(UnitType::pressure,              1)},
-    {"W",    Unit(UnitType::power,                 1)},
-    {"degC", Unit(UnitType::temperature,           1, 273.15)},
+        Unit("C",    UnitType::electric_charge,           1),
+        Unit("F",    UnitType::capacitance,               1),
+        Unit("H",    UnitType::inductance,                1),
+        Unit("Hz",   UnitType::frequency,                 1),
+        Unit("J",    UnitType::energy,                    1),
+        Unit("N",    UnitType::force,                     1),
+        Unit("ohm",  UnitType::electric_resistance,       1),
+        Unit("Pa",   UnitType::pressure,                  1),
+        Unit("S",    UnitType::electric_conductance,      1),
+        Unit("T",    UnitType::magnetic_flux_density,     1),
+        Unit("V",    UnitType::electric_potential,        1),
+        Unit("W",    UnitType::power,                     1),
+        Unit("Wb",   UnitType::magnetic_flux,             1),
 
-    /* Non-fundamental metric units */
-    {"min",  Unit(UnitType::time,                  60)},
-    {"h",    Unit(UnitType::time,                  60 * 60)},
-    {"d",    Unit(UnitType::time,                  24 * 60 * 60)},
-    {"L",    Unit(UnitType::volume,                1.0e-3)},
+        Unit("degC", UnitType::celsius_fahrenheit_temperature,
+            1, 273.15),
 
-    /* Non-metric units */
-    {"lbf",  Unit(UnitType::force,                 4.448222e+0)},
-    {"in",   Unit(UnitType::length,                2.54e-2)},
-    {"ft",   Unit(UnitType::length,                12 * 2.54e-2)},
-    {"lbm",  Unit(UnitType::mass,                  4.5359237e-1)},
-    {"degF", Unit(UnitType::temperature,           5 / 9.0, 255.372)}
+        Unit("min",  UnitType::time,                      60),
+        Unit("h",    UnitType::time,                      60 * 60),
+        Unit("d",    UnitType::time,                      24 * 60 * 60),
+        Unit("L",    UnitType::volume,                    1.0e-3),
+
+        Unit("ft",   UnitType::length,                    12 * 2.54e-2),
+        Unit("in",   UnitType::length,                    2.54e-2),
+        Unit("lbf",  UnitType::force,                     4.448222e+0),
+        Unit("lbm",  UnitType::mass,                      4.5359237e-1),
+
+        Unit("degF", UnitType::celsius_fahrenheit_temperature,
+            5 / 9.0, 255.372)
+    });
+    return ut;
+}
+
+const std::set<std::string> prefixable_units {
+    "m", "g", "s", "A", "K", "mol", "cd",
+    "C", "F", "H", "Hz", "J", "N", "ohm", "Pa", "S", "T", "V", "W", "Wb",
+    "L"
 };
 
-const std::set<std::string> non_prefixable_units {
-    "degC", "min", "h", "d", "lbf", "in", "ft", "lbm", "degF"
+const std::set<std::string> imperial_units {
+    "ft", "in", "lbf", "lbm", "degF"
 };
+
+const std::map<std::string, int> &si_prefix_exponents() {
+    static const std::map<std::string, int> spe {
+        {"y",  -24}, {"z",  -21}, {"a",  -18}, {"f",  -15},
+        {"p",  -12}, {"n",   -9}, {"u",   -6}, {"m",   -3},
+        {"c",   -2}, {"d",   -1}, {"da",  +1}, {"h",   +2},
+        {"k",   +3}, {"M",   +6}, {"G",   +9}, {"T",  +12},
+        {"P",  +15}, {"E",  +18}, {"Z",  +21}, {"Y",  +24}
+    };
+    return spe;
+}
 
 Unit Unit::parse_simple(const std::string &str) {
-    auto unit_it = unit_values.find(str);
-    if (unit_it != unit_values.end()) {
+    auto unit_it = unit_table().units_by_abbrev.find(str);
+    if (unit_it != unit_table().units_by_abbrev.end()) {
         return unit_it->second;
     }
 
-    for (const auto &pair : unit_values) {
-        if (str.length() < pair.first.length() ||
-                str.substr(str.length() - pair.first.length()) != pair.first) {
+    for (const auto &prefix_pair : si_prefix_exponents) {
+        if (prefix_pair.first != str.substr(0, prefix_pair.first.length())) {
             continue;
         }
-        auto prefix_it = si_prefix_exponents.find(
-            str.substr(0, str.length() - pair.first.length()));
-        if (prefix_it == si_prefix_exponents.end()) {
+        std::string unit_str = str.substr(prefix_pair.first.length());
+        unit_it = unit_table().units_by_abbrev.find(unit_str);
+        if (unit_it == unit_table().units_by_abbrev.end()) {
             continue;
         }
-        if (non_prefixable_units.count(pair.first)) {
-            throw UnitParseError("Unit '" + pair.first + "' may not be used "
-                "with SI prefixes.");
+        if (!prefixable_units.count(unit_str)) {
+            throw UnitParseError("Unit '" + unit_str + "' may not be used with "
+                "SI prefixes.");
         }
-        Unit unit = pair.second;
-        unit.unit_in_si *= pow(10, prefix_it->second);
-        return unit;
+        return Unit(
+            prefix_pair.first + unit_it->second.abbrev,
+            unit_it->second.type,
+            unit_it->second.unit_in_si * pow(10, prefix_pair.second));
     }
 
     if (str == "lb") {
@@ -147,13 +205,18 @@ Unit Unit::parse_simple(const std::string &str) {
             "explicitly clarify whether you mean pound-force or pound-mass.");
     }
 
+    if (str.find(' ') != std::string::npos ||
+            str.find('^') != std::string::npos) {
+        throw UnitParseError("Expected a single unit, not a compound unit.");
+    }
+
     std::stringstream msg;
     msg << "Unsupported unit '" << str << "'. Supported units are: ";
-    for (const auto &pair : unit_values) {
-        if (pair.first != unit_values.begin()->first) {
+    for (const Unit &unit : unit_table().all_units) {
+        if (unit.abbrev != unit_table().all_units[0].abbrev) {
             msg << ", ";
         }
-        msg << "'" << pair.first << "'";
+        msg << "'" << unit.abbrev << "'";
     }
     msg << ".";
     throw UnitParseError(msg.str());
@@ -161,7 +224,7 @@ Unit Unit::parse_simple(const std::string &str) {
 
 Unit Unit::parse_compound(const std::string &str) {
     if (str == "dimensionless") {
-        return Unit(UnitType::dimensionless, 1);
+        return Unit("dimensionless", UnitType::dimensionless, 1);
     }
 
     static const std::string space_msg = "Unexpected extra space(s) in unit "
@@ -255,19 +318,23 @@ Unit Unit::parse_compound(const std::string &str) {
         return parse_simple(units.begin()->first);
     }
 
-    Unit combined_unit(UnitType::dimensionless, 1);
+    Unit combined_unit(str, UnitType::dimensionless, 1);
     for (const auto &pair : units) {
         Unit unit = parse_simple(pair.first);
         if (unit.zero_in_si != 0) {
             throw UnitParseError("Unit '" + pair.first + "' cannot be "
                 "combined with other units or raised to a power, because the "
-                "zero-point is arbitrary so this wouldn't make sense.");
+                "zero-point is relative to a reference, so this wouldn't make "
+                "sense.");
         }
-        combined_unit.type.length_power += unit.type.length_power * pair.second;
-        combined_unit.type.mass_power += unit.type.mass_power * pair.second;
-        combined_unit.type.time_power += unit.type.time_power * pair.second;
-        combined_unit.type.temperature_power +=
-            unit.type.temperature_power * pair.second;
+        combined_unit.type.length_power +=
+            unit.type.length_power * pair.second;
+        combined_unit.type.mass_power +=
+            unit.type.mass_power * pair.second;
+        combined_unit.type.time_power +=
+            unit.type.time_power * pair.second;
+        combined_unit.type.thermodynamic_temperature_power +=
+            unit.type.thermodynamic_temperature_power * pair.second;
         combined_unit.type.electric_current_power +=
             unit.type.electric_current_power * pair.second;
         combined_unit.type.amount_of_substance_power +=
@@ -278,6 +345,98 @@ Unit Unit::parse_compound(const std::string &str) {
     }
 
     return combined_unit;
+}
+
+UnitSystem UnitSystem::parse(
+    const std::string &length_str,
+    const std::string &mass_or_force_str,
+    const std::string &time_str,
+    const std::string &temperature_str
+) {
+    UnitSystem system;
+
+    Unit length_unit = Unit::parse_simple(length_str);
+    Unit mass_or_force_unit = Unit::parse_simple(mass_str);
+    Unit time_unit = Unit::parse_simple(time_str);
+    Unit temperature_unit = Unit::parse_simple(temperature_str);
+
+    if (length_str == "in" || length_str == "ft") {
+        system.style = Style::Imperial;
+    } else {
+        system.style = Style::Metric;
+    }
+
+    if (length_unit.type != UnitType::length) {
+        throw UnitParseError("'" + length_str + "' is not a unit of length.");
+    }
+    system.length_unit_in_si = length_unit.unit_in_si;
+
+    if (time_unit.type != UnitType::time) {
+        throw UnitParseError("'" + time_str + "' is not a unit of time.");
+    }
+    system.time_unit_in_si = time_unit.unit_in_si;
+
+    if (mass_or_force_unit.type == UnitType::mass) {
+        system.mass_unit_in_si = mass_or_force_unit.unit_in_si;
+    } else if (mass_or_force_unit.type == UnitType::force) {
+        system.mass_unit_in_si = mass_or_force_unit.unit_in_si
+            / (system.length_unit_in_si / pow(system.time_unit_in_si, 2));
+    } else {
+        throw UnitParseError("'" + mass_or_force_str +
+            "' is not a unit of mass or force.");
+    }
+
+    if (temperature_unit.type != UnitType::temperature) {
+        throw UnitParseError("'" + temperature_str +
+            "' is not a unit of temperature.");
+    }
+    system.temperature_unit_in_si = temperature_unit.unit_in_si;
+    system.temperature_zero_in_si = temperature_unit.zero_in_si;
+
+    return system;
+}
+
+double UnitSystem::convert_from_unit(double value_in_unit, Unit unit) const {
+    double value = value_in_unit;
+    value *= unit.unit_in_si;
+    if (unit.type.arbitrary_zero) {
+        assert(unit.type == UnitType::celsius_fahrenheit_temperature);
+        value += unit.zero_in_si;
+        value -= temperature_zero_in_si;
+    }
+    value /= pow(length_unit_in_si, unit.type.length_power);
+    value /= pow(mass_unit_in_si, unit.type.mass_power);
+    value /= pow(time_unit_in_si, unit.type.time_power);
+    value /= pow(temperature_unit_in_si,
+        unit.type.thermodynamic_temperature_power);
+    return value;
+}
+
+double UnitSystem::convert_to_unit(double value_in_system, Unit unit) const {
+    double value = value_in_system;
+    value *= pow(length_unit_in_si, unit.type.length_power);
+    value *= pow(mass_unit_in_si, unit.type.mass_power);
+    value *= pow(time_unit_in_si, unit.type.time_power);
+    value *= pow(temperature_unit_in_si,
+        unit.type.thermodynamic_temperature_power);
+    if (unit.type.arbitrary_zero) {
+        assert(unit.type == UnitType::celsius_fahrenheit_temperature);
+        value -= temperature_zero_in_si;
+        value += unit.zero_in_si;
+    }
+    value /= unit.unit_in_si;
+    return value;
+}
+
+Unit UnitSystem::choose_unit(
+    UnitType type,
+    double value_for_scale_in_system
+) const {
+    double value_for_scale_in_si =
+        convert_to_unit(value_for_scale_in_system, Unit(type, 1.0));
+    if (style == Style::Imperial) {
+
+    }
 }
 
 } /* namespace os2cx */
--- core/units.hpp
+++ core/units.hpp
@@ -1,6 +1,7 @@
 #ifndef OS2CX_UNITS_HPP_
 #define OS2CX_UNITS_HPP_
 
+#include <pair>
 #include <stdexcept>
 #include <string>
 
@@ -8,38 +9,42 @@ namespace os2cx {
 
 class UnitType {
 public:
-    static const UnitType dimensionless;
+    static UnitType dimensionless();
 
-    static const UnitType length;
-    static const UnitType mass;
-    static const UnitType time;
-    static const UnitType electric_current;
-    static const UnitType temperature;
-    static const UnitType amount_of_substance;
-    static const UnitType luminous_intensity;
+    static UnitType length();
+    static UnitType mass();
+    static UnitType time();
+    static UnitType electric_current();
+    static UnitType thermodynamic_temperature();
+    static UnitType amount_of_substance();
+    static UnitType luminous_intensity();
 
-    static const UnitType acceleration;
-    static const UnitType area;
-    static const UnitType capacitance;
-    static const UnitType density;
-    static const UnitType dynamic_viscosity;
-    static const UnitType electric_charge;
-    static const UnitType electric_conductance;
-    static const UnitType electric_potential;
-    static const UnitType electric_resistance;
-    static const UnitType energy;
-    static const UnitType force;
-    static const UnitType frequency;
-    static const UnitType inductance;
-    static const UnitType kinematic_viscosity;
-    static const UnitType magnetic_flux;
-    static const UnitType magnetic_flux_density;
-    static const UnitType power;
-    static const UnitType pressure;
-    static const UnitType speed;
-    static const UnitType volume;
+    static UnitType acceleration();
+    static UnitType area();
+    static UnitType capacitance();
+    static UnitType density();
+    static UnitType dynamic_viscosity();
+    static UnitType electric_charge();
+    static UnitType electric_conductance();
+    static UnitType electric_potential();
+    static UnitType electric_resistance();
+    static UnitType energy();
+    static UnitType force();
+    static UnitType frequency();
+    static UnitType inductance();
+    static UnitType kinematic_viscosity();
+    static UnitType magnetic_flux();
+    static UnitType magnetic_flux_density();
+    static UnitType power();
+    static UnitType pressure();
+    static UnitType speed();
+    static UnitType volume();
 
-    UnitType(int lp, int mp, int tp, int ecp, int tep, int aosp, int lip);
+    static UnitType celsius_fahrenheit_temperature();
+
+    UnitType(
+        int lp, int mp, int tp, int ecp, int ttp, int aosp, int lip,
+        bool az = false);
 
     bool operator==(const UnitType &other) const;
     bool operator!=(const UnitType &other) const;
@@ -48,9 +53,13 @@ public:
     int mass_power;
     int time_power;
     int electric_current_power;
-    int temperature_power;
+    int thermodynamic_temperature_power;
     int amount_of_substance_power;
     int luminous_intensity_power;
+
+    /* 'arbitrary_zero' is true for Celsius and Fahrenheit, false for Kelvin and
+    for non-temperature units. */
+    bool arbitrary_zero;
 };
 
 class UnitParseError : public std::invalid_argument {
@@ -58,21 +67,51 @@ public:
     UnitParseError(const std::string &msg) : std::invalid_argument(msg) { }
 };
 
-/* Unit represents a unit of measure, like "inches" or "gigapascals". The
-constructor takes a string (typically user input) and parses it. Note that
-there's no way to go in the opposite direction; you can't generate a string
-representation of a Unit, because there are too many awkward corner cases. */
+/* Unit represents a unit of measure, like "inches" or "gigapascals". */
 class Unit {
 public:
+    /* Invariants:
+    parse_simple(str).abbrev == str       [if it doesn't throw an exception]
+    parse_compound(str).abbrev == str     [if it doesn't throw an exception]
+    parse_simple(unit.abbrev) == unit     [if it doesn't throw an exception]
+    parse_compound(unit.abbrev) == unit   [will never throw an exception]
+    */
     static Unit parse_simple(const std::string &str);
     static Unit parse_compound(const std::string &str);
 
-    Unit(UnitType t, double uis, double zis = 0.0) :
-        type(t), unit_in_si(uis), zero_in_si(zis) { }
+    Unit(const std::string &a, UnitType t, double uis, double zis = 0.0) :
+        abbrev(n), type(t), unit_in_si(uis), zero_in_si(zis) { }
+
+    /* Note that multiple equivalent units can have the same 'abbrev'; for
+    example, 'mL' and 'cm^3'. */
+    std::string abbrev;
 
     UnitType type;
     double unit_in_si;
     double zero_in_si; /* Always zero except for Celsius and Fahrenheit */
+};
+
+class UnitSystem {
+public:
+    enum class Style {Metric, Imperial};
+
+    static UnitSystem parse(
+        const std::string &length_str,
+        const std::string &mass_or_force_str,
+        const std::string &time_str,
+        const std::string &temperature_str);
+
+    double convert_from_unit(double value_in_unit, Unit unit) const;
+    double convert_to_unit(double value_in_unit, Unit unit) const;
+
+    Unit choose_unit(UnitType type, double value_for_scale_in_system) const;
+
+    Style style;
+    double length_unit_in_si;
+    double mass_unit_in_si;
+    double time_unit_in_si;
+    double temperature_unit_in_si;
+    double temperature_zero_in_si;
 };
 
 } /* namespace os2cx */
